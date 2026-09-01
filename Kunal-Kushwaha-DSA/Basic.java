8a3fc4d68b0283e77b823b6026a370b9130d7f31
Noah-sam <noahsam1324watkins@gamil.com>
2026-09-01 22:03:07 +0530

    Update in linear sreach

---
+++ Kunal-Kushwaha-DSA/Basic.java
@@ -0,0 +1,26 @@
+public class Basic {
+                public static void main(String[] args) {
+	    int[] nums = {23, 45, 1, 2, 8, 19, -3, 16, -11, 28};
+	    int target = 19;
+	    boolean ans = linearSearch3(nums, target);
+        System.out.println(ans);
+    }
+
+    // search the target and return true or false
+    static boolean linearSearch3(int[] arr, int target) {
+        if (arr.length == 0) {
+            return false;
+        }
+
+        // run a for loop
+        for (int element : arr) {
+            if (element == target) {
+                return true;
+            }
+        }
+        // this line will execute if none of the return statements above have executed
+        // hence the target not found
+        return false;
+
+    }
+}
